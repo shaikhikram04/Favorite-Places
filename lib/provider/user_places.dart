602a8488f51647b3d9d14058d6d9d032bd296cf1
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-07-30 20:58:40 +0530

    loading data from tha sql database

--- lib/provider/user_places.dart
+++ lib/provider/user_places.dart
@@ -12,29 +12,55 @@ import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart' as sql;
 import 'package:sqflite/sqlite_api.dart';
 
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address, TEXT)');
+    },
+    version: 1,
+  );
+
+  return db;
+}
+
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
+
+  void loadPlaces() async {
+    final db = await _getDatabase();
+    //* get data from database
+    final data = await db.query('user_places');
+
+    final places = data
+        .map((row) => Place(
+              id: row['id'] as String,
+              title: row['title'] as String,
+              image: File(row['image'] as String),
+              location: PlaceLocation(
+                  latitude: row['lat'] as double,
+                  longitude: row['lng'] as double,
+                  address: row['address'] as String),
+            ))
+        .toList();
+
+    state = places;
+  }
 
   void addPlace(String title, File image, PlaceLocation location) async {
     final appDir = await syspaths
         .getApplicationDocumentsDirectory(); // getting directory in which app data are store
     final fileName =
-    path.basename(image.path); // getting name of file of a image
+        path.basename(image.path); // getting name of file of a image
     final copiedImage = await image.copy(
-        '${appDir
-            .path}/$fileName'); // copy image on that path into a directory of filename
+        '${appDir.path}/$fileName'); // copy image on that path into a directory of filename
 
-    final newPlace = Place(title: title, image: image, location: location);
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
 
-    final dbPath = await sql.getDatabasesPath();
-    final db = await sql.openDatabase(
-      path.join(dbPath, 'places.db'),
-      onCreate: (db, version) {
-        return db.execute(
-            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address, TEXT)');
-      },
-      version: 1,
-    );
+    final db = await _getDatabase();
     db.insert('user_places', {
       'id': newPlace.id,
       'title': newPlace.title,
@@ -49,6 +75,6 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
 }
 
 final userPlacesProvider =
-StateNotifierProvider<UserPlacesNotifier, List<Place>>(
-      (ref) => UserPlacesNotifier(),
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+  (ref) => UserPlacesNotifier(),
 );
